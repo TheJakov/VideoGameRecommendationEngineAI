429e572b17eaf3f3e57a5257089db430cb0026b2
Jakov Kristović <37014410+jkristovi@users.noreply.github.com>
2019-12-22 19:06:01 +0100

    Azurirane forme - pitanje 1 i 2

--- sustavZaPreporukuVideoIgara/Forme/DrugoPitanje.Designer.cs
+++ sustavZaPreporukuVideoIgara/Forme/DrugoPitanje.Designer.cs
@@ -34,17 +34,22 @@
             this.btnOdustani = new System.Windows.Forms.Button();
             this.btnSljedece = new System.Windows.Forms.Button();
             this.btnPrethodno = new System.Windows.Forms.Button();
+            this.cbAdults = new System.Windows.Forms.CheckBox();
+            this.cbMature = new System.Windows.Forms.CheckBox();
+            this.cbTeen = new System.Windows.Forms.CheckBox();
+            this.cbEveryone = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblDrugoPitanje
             // 
             this.lblDrugoPitanje.AutoSize = true;
             this.lblDrugoPitanje.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDrugoPitanje.Location = new System.Drawing.Point(199, 35);
+            this.lblDrugoPitanje.Location = new System.Drawing.Point(42, 42);
             this.lblDrugoPitanje.Name = "lblDrugoPitanje";
-            this.lblDrugoPitanje.Size = new System.Drawing.Size(400, 29);
+            this.lblDrugoPitanje.Size = new System.Drawing.Size(302, 29);
             this.lblDrugoPitanje.TabIndex = 5;
-            this.lblDrugoPitanje.Text = "Ovdje će se postaviti drugo pitanje ?";
+            this.lblDrugoPitanje.Text = "Kakav ESRB rejting tražiš ?";
             // 
             // lblBrojPitanja
             // 
@@ -86,12 +91,71 @@
             this.btnPrethodno.UseVisualStyleBackColor = true;
             this.btnPrethodno.Click += new System.EventHandler(this.btnPrethodno_Click);
             // 
+            // cbAdults
+            // 
+            this.cbAdults.AutoSize = true;
+            this.cbAdults.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbAdults.Location = new System.Drawing.Point(108, 266);
+            this.cbAdults.Name = "cbAdults";
+            this.cbAdults.Size = new System.Drawing.Size(89, 29);
+            this.cbAdults.TabIndex = 14;
+            this.cbAdults.Text = "Adults";
+            this.cbAdults.UseVisualStyleBackColor = true;
+            // 
+            // cbMature
+            // 
+            this.cbMature.AutoSize = true;
+            this.cbMature.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbMature.Location = new System.Drawing.Point(108, 215);
+            this.cbMature.Name = "cbMature";
+            this.cbMature.Size = new System.Drawing.Size(95, 29);
+            this.cbMature.TabIndex = 13;
+            this.cbMature.Text = "Mature";
+            this.cbMature.UseVisualStyleBackColor = true;
+            // 
+            // cbTeen
+            // 
+            this.cbTeen.AutoSize = true;
+            this.cbTeen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbTeen.Location = new System.Drawing.Point(108, 168);
+            this.cbTeen.Name = "cbTeen";
+            this.cbTeen.Size = new System.Drawing.Size(80, 29);
+            this.cbTeen.TabIndex = 12;
+            this.cbTeen.Text = "Teen";
+            this.cbTeen.UseVisualStyleBackColor = true;
+            // 
+            // cbEveryone
+            // 
+            this.cbEveryone.AutoSize = true;
+            this.cbEveryone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbEveryone.Location = new System.Drawing.Point(108, 119);
+            this.cbEveryone.Name = "cbEveryone";
+            this.cbEveryone.Size = new System.Drawing.Size(117, 29);
+            this.cbEveryone.TabIndex = 11;
+            this.cbEveryone.Text = "Everyone";
+            this.cbEveryone.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 350);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 25);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Moguće označiti samo 1 opciju. ";
+            // 
             // DrugoPitanje
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(804, 542);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbAdults);
+            this.Controls.Add(this.cbMature);
+            this.Controls.Add(this.cbTeen);
+            this.Controls.Add(this.cbEveryone);
             this.Controls.Add(this.btnPrethodno);
             this.Controls.Add(this.btnSljedece);
             this.Controls.Add(this.btnOdustani);
@@ -117,5 +181,10 @@
         private System.Windows.Forms.Button btnOdustani;
         private System.Windows.Forms.Button btnSljedece;
         private System.Windows.Forms.Button btnPrethodno;
+        private System.Windows.Forms.CheckBox cbAdults;
+        private System.Windows.Forms.CheckBox cbMature;
+        private System.Windows.Forms.CheckBox cbTeen;
+        private System.Windows.Forms.CheckBox cbEveryone;
+        private System.Windows.Forms.Label label1;
     }
 }
--- sustavZaPreporukuVideoIgara/Forme/PrvoPitanje.Designer.cs
+++ sustavZaPreporukuVideoIgara/Forme/PrvoPitanje.Designer.cs
@@ -33,6 +33,13 @@
             this.lblPrvoPitanje = new System.Windows.Forms.Label();
             this.lblBrojPitanja = new System.Windows.Forms.Label();
             this.btnSljedece = new System.Windows.Forms.Button();
+            this.cbShooter = new System.Windows.Forms.CheckBox();
+            this.cbRpg = new System.Windows.Forms.CheckBox();
+            this.cbSimulation = new System.Windows.Forms.CheckBox();
+            this.cbMoba = new System.Windows.Forms.CheckBox();
+            this.cbPlatformer = new System.Windows.Forms.CheckBox();
+            this.cbBattleRoyale = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnOdustani
@@ -49,11 +56,11 @@
             // 
             this.lblPrvoPitanje.AutoSize = true;
             this.lblPrvoPitanje.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPrvoPitanje.Location = new System.Drawing.Point(219, 36);
+            this.lblPrvoPitanje.Location = new System.Drawing.Point(42, 42);
             this.lblPrvoPitanje.Name = "lblPrvoPitanje";
-            this.lblPrvoPitanje.Size = new System.Drawing.Size(384, 29);
+            this.lblPrvoPitanje.Size = new System.Drawing.Size(238, 29);
             this.lblPrvoPitanje.TabIndex = 4;
-            this.lblPrvoPitanje.Text = "Ovdje će se postaviti prvo pitanje ?";
+            this.lblPrvoPitanje.Text = "Kakav tip igre tražiš ?";
             // 
             // lblBrojPitanja
             // 
@@ -75,12 +82,95 @@
             this.btnSljedece.UseVisualStyleBackColor = true;
             this.btnSljedece.Click += new System.EventHandler(this.btnSljedece_Click);
             // 
+            // cbShooter
+            // 
+            this.cbShooter.AutoSize = true;
+            this.cbShooter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbShooter.Location = new System.Drawing.Point(130, 128);
+            this.cbShooter.Name = "cbShooter";
+            this.cbShooter.Size = new System.Drawing.Size(103, 29);
+            this.cbShooter.TabIndex = 7;
+            this.cbShooter.Text = "Shooter";
+            this.cbShooter.UseVisualStyleBackColor = true;
+            // 
+            // cbRpg
+            // 
+            this.cbRpg.AutoSize = true;
+            this.cbRpg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbRpg.Location = new System.Drawing.Point(130, 177);
+            this.cbRpg.Name = "cbRpg";
+            this.cbRpg.Size = new System.Drawing.Size(75, 29);
+            this.cbRpg.TabIndex = 8;
+            this.cbRpg.Text = "RPG";
+            this.cbRpg.UseVisualStyleBackColor = true;
+            // 
+            // cbSimulation
+            // 
+            this.cbSimulation.AutoSize = true;
+            this.cbSimulation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbSimulation.Location = new System.Drawing.Point(130, 275);
+            this.cbSimulation.Name = "cbSimulation";
+            this.cbSimulation.Size = new System.Drawing.Size(125, 29);
+            this.cbSimulation.TabIndex = 10;
+            this.cbSimulation.Text = "Simulation";
+            this.cbSimulation.UseVisualStyleBackColor = true;
+            // 
+            // cbMoba
+            // 
+            this.cbMoba.AutoSize = true;
+            this.cbMoba.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbMoba.Location = new System.Drawing.Point(130, 224);
+            this.cbMoba.Name = "cbMoba";
+            this.cbMoba.Size = new System.Drawing.Size(94, 29);
+            this.cbMoba.TabIndex = 9;
+            this.cbMoba.Text = "MOBA";
+            this.cbMoba.UseVisualStyleBackColor = true;
+            // 
+            // cbPlatformer
+            // 
+            this.cbPlatformer.AutoSize = true;
+            this.cbPlatformer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbPlatformer.Location = new System.Drawing.Point(130, 372);
+            this.cbPlatformer.Name = "cbPlatformer";
+            this.cbPlatformer.Size = new System.Drawing.Size(122, 29);
+            this.cbPlatformer.TabIndex = 12;
+            this.cbPlatformer.Text = "Platformer";
+            this.cbPlatformer.UseVisualStyleBackColor = true;
+            // 
+            // cbBattleRoyale
+            // 
+            this.cbBattleRoyale.AutoSize = true;
+            this.cbBattleRoyale.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbBattleRoyale.Location = new System.Drawing.Point(130, 323);
+            this.cbBattleRoyale.Name = "cbBattleRoyale";
+            this.cbBattleRoyale.Size = new System.Drawing.Size(148, 29);
+            this.cbBattleRoyale.TabIndex = 11;
+            this.cbBattleRoyale.Text = "Battle Royale";
+            this.cbBattleRoyale.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 421);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 25);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Moguće označiti max 2 opcije. ";
+            // 
             // PrvoPitanje
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(804, 542);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbPlatformer);
+            this.Controls.Add(this.cbBattleRoyale);
+            this.Controls.Add(this.cbSimulation);
+            this.Controls.Add(this.cbMoba);
+            this.Controls.Add(this.cbRpg);
+            this.Controls.Add(this.cbShooter);
             this.Controls.Add(this.btnSljedece);
             this.Controls.Add(this.lblBrojPitanja);
             this.Controls.Add(this.lblPrvoPitanje);
@@ -104,5 +194,12 @@
         private System.Windows.Forms.Label lblPrvoPitanje;
         private System.Windows.Forms.Label lblBrojPitanja;
         private System.Windows.Forms.Button btnSljedece;
+        private System.Windows.Forms.CheckBox cbShooter;
+        private System.Windows.Forms.CheckBox cbRpg;
+        private System.Windows.Forms.CheckBox cbSimulation;
+        private System.Windows.Forms.CheckBox cbMoba;
+        private System.Windows.Forms.CheckBox cbPlatformer;
+        private System.Windows.Forms.CheckBox cbBattleRoyale;
+        private System.Windows.Forms.Label label1;
     }
 }
