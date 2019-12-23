560f327b9e15ccb9b0f98d5e3c8fe83acaa4962c
Jakov Kristović <37014410+jkristovi@users.noreply.github.com>
2019-12-23 19:30:53 +0100

    Azurirana zavrsna forma aplikacije
    Azurirana zavrsna forma aplikacije

--- sustavZaPreporukuVideoIgara/Forme/Rezultat.Designer.cs
+++ sustavZaPreporukuVideoIgara/Forme/Rezultat.Designer.cs
@@ -40,16 +40,24 @@
             this.pictureBoxGold = new System.Windows.Forms.PictureBox();
             this.pictureBoxSilver = new System.Windows.Forms.PictureBox();
             this.pictureBoxBronze = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelAIresult = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGold)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSilver)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBronze)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblDrugoPitanje
             // 
             this.lblDrugoPitanje.AutoSize = true;
             this.lblDrugoPitanje.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDrugoPitanje.Location = new System.Drawing.Point(42, 42);
+            this.lblDrugoPitanje.Location = new System.Drawing.Point(42, 15);
             this.lblDrugoPitanje.Name = "lblDrugoPitanje";
             this.lblDrugoPitanje.Size = new System.Drawing.Size(223, 29);
             this.lblDrugoPitanje.TabIndex = 27;
@@ -59,7 +67,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(42, 93);
+            this.label1.Location = new System.Drawing.Point(42, 66);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(652, 29);
             this.label1.TabIndex = 28;
@@ -69,7 +77,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(74, 429);
+            this.label5.Location = new System.Drawing.Point(74, 392);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(635, 29);
             this.label5.TabIndex = 32;
@@ -79,7 +87,7 @@
             // 
             this.label1st.AutoSize = true;
             this.label1st.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1st.Location = new System.Drawing.Point(184, 171);
+            this.label1st.Location = new System.Drawing.Point(184, 144);
             this.label1st.Name = "label1st";
             this.label1st.Size = new System.Drawing.Size(153, 25);
             this.label1st.TabIndex = 33;
@@ -89,7 +97,7 @@
             // 
             this.label2nd.AutoSize = true;
             this.label2nd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2nd.Location = new System.Drawing.Point(184, 251);
+            this.label2nd.Location = new System.Drawing.Point(184, 224);
             this.label2nd.Name = "label2nd";
             this.label2nd.Size = new System.Drawing.Size(185, 25);
             this.label2nd.TabIndex = 34;
@@ -99,7 +107,7 @@
             // 
             this.label3rd.AutoSize = true;
             this.label3rd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3rd.Location = new System.Drawing.Point(184, 338);
+            this.label3rd.Location = new System.Drawing.Point(184, 311);
             this.label3rd.Name = "label3rd";
             this.label3rd.Size = new System.Drawing.Size(217, 25);
             this.label3rd.TabIndex = 35;
@@ -107,7 +115,7 @@
             // 
             // btnZatvori
             // 
-            this.btnZatvori.Location = new System.Drawing.Point(405, 477);
+            this.btnZatvori.Location = new System.Drawing.Point(410, 778);
             this.btnZatvori.Name = "btnZatvori";
             this.btnZatvori.Size = new System.Drawing.Size(125, 42);
             this.btnZatvori.TabIndex = 49;
@@ -117,7 +125,7 @@
             // 
             // btnPocetna
             // 
-            this.btnPocetna.Location = new System.Drawing.Point(260, 477);
+            this.btnPocetna.Location = new System.Drawing.Point(265, 778);
             this.btnPocetna.Name = "btnPocetna";
             this.btnPocetna.Size = new System.Drawing.Size(125, 42);
             this.btnPocetna.TabIndex = 50;
@@ -128,7 +136,7 @@
             // pictureBoxGold
             // 
             this.pictureBoxGold.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxGold.Image")));
-            this.pictureBoxGold.Location = new System.Drawing.Point(79, 149);
+            this.pictureBoxGold.Location = new System.Drawing.Point(79, 122);
             this.pictureBoxGold.Name = "pictureBoxGold";
             this.pictureBoxGold.Size = new System.Drawing.Size(99, 87);
             this.pictureBoxGold.TabIndex = 51;
@@ -137,7 +145,7 @@
             // pictureBoxSilver
             // 
             this.pictureBoxSilver.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxSilver.Image")));
-            this.pictureBoxSilver.Location = new System.Drawing.Point(79, 232);
+            this.pictureBoxSilver.Location = new System.Drawing.Point(79, 205);
             this.pictureBoxSilver.Name = "pictureBoxSilver";
             this.pictureBoxSilver.Size = new System.Drawing.Size(99, 86);
             this.pictureBoxSilver.TabIndex = 52;
@@ -146,18 +154,94 @@
             // pictureBoxBronze
             // 
             this.pictureBoxBronze.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxBronze.Image")));
-            this.pictureBoxBronze.Location = new System.Drawing.Point(79, 314);
+            this.pictureBoxBronze.Location = new System.Drawing.Point(79, 287);
             this.pictureBoxBronze.Name = "pictureBoxBronze";
             this.pictureBoxBronze.Size = new System.Drawing.Size(99, 85);
             this.pictureBoxBronze.TabIndex = 53;
             this.pictureBoxBronze.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 422);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(702, 29);
+            this.label2.TabIndex = 54;
+            this.label2.Text = "_____________________________________________________";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(44, 465);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(704, 29);
+            this.label3.TabIndex = 55;
+            this.label3.Text = "S druge strane, naš ML algoritam (koji nije još uvijek najbistriji) je";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(44, 496);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(692, 29);
+            this.label4.TabIndex = 56;
+            this.label4.Text = "također pokušao zaključiti na temelju podataka koje si nam dao,";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(44, 528);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(478, 29);
+            this.label6.TabIndex = 57;
+            this.label6.Text = "te je on zaključio da je za tebe najbolja igra:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(79, 592);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(99, 85);
+            this.pictureBox1.TabIndex = 58;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelAIresult
+            // 
+            this.labelAIresult.AutoSize = true;
+            this.labelAIresult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAIresult.Location = new System.Drawing.Point(184, 619);
+            this.labelAIresult.Name = "labelAIresult";
+            this.labelAIresult.Size = new System.Drawing.Size(249, 25);
+            this.labelAIresult.TabIndex = 59;
+            this.labelAIresult.Text = "Text to override too too too!";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(49, 705);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(702, 29);
+            this.label7.TabIndex = 60;
+            this.label7.Text = "Nadamo se da ni naš ML algoritam nije ponudio skroz lošu ideju.";
             // 
             // Rezultat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(804, 542);
+            this.ClientSize = new System.Drawing.Size(804, 871);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.labelAIresult);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBoxBronze);
             this.Controls.Add(this.pictureBoxSilver);
             this.Controls.Add(this.pictureBoxGold);
@@ -171,15 +255,16 @@
             this.Controls.Add(this.lblDrugoPitanje);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(822, 589);
+            this.MaximumSize = new System.Drawing.Size(822, 918);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(822, 589);
+            this.MinimumSize = new System.Drawing.Size(822, 918);
             this.Name = "Rezultat";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Rezultat";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGold)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSilver)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBronze)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -198,5 +283,12 @@
         private System.Windows.Forms.PictureBox pictureBoxGold;
         private System.Windows.Forms.PictureBox pictureBoxSilver;
         private System.Windows.Forms.PictureBox pictureBoxBronze;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelAIresult;
+        private System.Windows.Forms.Label label7;
     }
 }
